a98dacb06674ec348bd7d1dbf96d03e749a86a6d
George kapoya <gkapoya@gmail.com>
2019-08-15 14:52:56 +0300

    enable django admin for user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,39 +1,41 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
         """create a new user profiles"""
         if not email:
-            raise ValueError('user must have a valid email address')
+            # raise ValueError('user must have a valid email address')
+            raise ValueError('User must have an email address')
 
-            email = self.normalize_email(email)
-            user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            user.set_password(password)
-            user.save(using=self._db)
+        return user
 
-            return user
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            def create_superuser(self, email, name, password):
-                """create a new superuser"""
-                user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-                user.is_superuser = True
-                user.is_staff = True
-                user.save(using=self._db)
+        return user
 
-                return user
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
@@ -44,10 +46,10 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """retrieve full name of user"""
         return self.name
 
-        def get_short_name(self):
-            """retrieve short name of user"""
-            return self.name
+    def get_short_name(self):
+        """retrieve short name of user"""
+        return self.name
 
-            def __str__(self):
-                """retrieve string representation of user"""
-                return self.email
+    def __str__(self):
+        """retrieve string representation of user"""
+        return self.email
